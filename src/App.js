ec4ac7dc56fa0d1f9940b503aa1e6def1d563ba2
TaylorDickens <justindickens17@gmail.com>
2020-05-05 15:55:40 -0400

    1st push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import 'antd/dist/antd.css';
+import Planets from './planets/Planets';
 
 function App() {
   return (
     <div>
-
+      <Planets />
     </div>
   );
 }
