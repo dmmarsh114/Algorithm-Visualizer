229d2cd68ead2356332f3b1009cc11ab2aad8778
Tristanoshier <tsoshier@bsu.edu>
2020-05-05 15:50:26 -0400

    add folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Tristan from './components/solarSystemPage/Tristan';
 
 function App() {
   return (
     <div>
-      <h1>Tristan</h1>
+      <h1>Planets App</h1>
+      <Tristan />
 
     </div>
   );
