0fd426cce8271a2622ad3d1a64599675f00d05e7
Tristanoshier <tsoshier@bsu.edu>
2020-05-06 13:36:34 -0400

    add solar system animation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
+import './css/main.css'
 import 'antd/dist/antd.css';
-import Planets from './components/solarSystemPage/Planets';
-import Tristan from './components/solarSystemPage/Tristan';
+// import Planets from './components/Taylor/Planets';
+// import Tristan from './components/Tristan/Tristan';
+import SolarSystemPage from './components/Tristan/SolarSystemPage';
+import LandingPage from './components/Tristan/LandingPage';
+import NextPage from './components/Tristan/NextPage';
 
 function App() {
   return (
     <div>
-      <h1>Tristan</h1>
+      {/* <h1>Tristan</h1>
       <Tristan />
       <h1>Taylor</h1>
-      <Planets />
+      <Planets /> */}
+      <LandingPage />
+      <SolarSystemPage />
+      <NextPage />
     </div>
   );
 }
