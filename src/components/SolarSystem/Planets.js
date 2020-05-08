5d89168c337409625ab2c514433105c9e8b6d528
Tristanoshier <tsoshier@bsu.edu>
2020-05-08 12:30:43 -0400

    abstract image and description object

---
+++ src/components/SolarSystem/Planets.js
@@ -0,0 +1,45 @@
+
+        let planets = {
+            Sun: {
+                description: 'The Sun is the star at the center of the Solar System. It is a nearly perfect sphere of hot plasma and is by far the most important source of energy for life on Earth.',
+                image: 'https://images-assets.nasa.gov/image/GSFC_20171208_Archive_e001435/GSFC_20171208_Archive_e001435~orig.jpg'
+            },
+            Mercury: {
+                description: 'Mercury is the smallest and innermost planet in the Solar System.  It is named after the Roman deity Mercury, the messenger of the gods.',
+                image: 'https://images-assets.nasa.gov/image/PIA11245/PIA11245~orig.jpg',  
+            },
+            Venus: {
+                description: 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty. As the second-brightest natural object in the night sky after the Moon, Venus can cast shadows and, rarely, is visible to the naked eye in broad daylight.',
+                image: 'https://images-assets.nasa.gov/image/PIA00270/PIA00270~orig.jpg',  
+            },
+            Earth: {
+                description: 'Earth is the third planet from the Sun and the only astronomical object known to harbor life. According to radiometric dating and other evidence, Earth formed over 4.5 billion years ago.',
+                image: 'https://images-assets.nasa.gov/image/PIA18033/PIA18033~orig.jpg',  
+            },
+            Mars: {
+                description: 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System after Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the "Red Planet".',
+                image: 'https://images-assets.nasa.gov/image/PIA03276/PIA03276~orig.jpg',  
+            },
+            Jupiter: {
+                description: 'Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant named after the Roman god Jupiter. When viewed from Earth, Jupiter is on average the third-brightest natural object in the night sky after the Moon and Venus. ',
+                image: 'https://images-assets.nasa.gov/image/PIA00343/PIA00343~orig.jpg',  
+            },
+            Saturn: {
+                description: 'Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine times that of Earth. Saturn is named after the Roman god of wealth and agriculture.',
+                image: 'https://images-assets.nasa.gov/image/PIA05380/PIA05380~orig.jpg',  
+            },
+            Uranus: {
+                description: 'Uranus is the seventh planet from the Sun. It is similar in composition to Neptune, and both have bulk chemical compositions which differ from that of the larger gas giants Jupiter and Saturn.',
+                image: 'https://images-assets.nasa.gov/image/PIA18182/PIA18182~orig.jpg',  
+            },
+            Neptune: {
+                description: 'Neptune is the eighth and farthest known planet from the Sun in the Solar System. In the Solar System, it is the fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. It is named after the Roman god of the sea.',
+                image: 'https://images-assets.nasa.gov/image/PIA01492/PIA01492~orig.jpg',  
+            }, 
+            Pluto: {
+                description: 'Pluto is an icy dwarf planet in the Kuiper belt, a ring of bodies beyond the orbit of Neptune. It was the first and the largest Kuiper belt object to be discovered.',
+                image: 'https://images-assets.nasa.gov/image/PIA19952/PIA19952~orig.jpg',  
+            }
+        }
+
+export default planets;
--- src/components/SolarSystem/SolarSystemCard.jsx
+++ src/components/SolarSystem/SolarSystemCard.jsx
@@ -1,40 +1,15 @@
 import React from 'react';
 import { Card } from 'antd';
+import planets from './Planets';
 
 const SolarSystemCard = (props) => {
 
-    let planetImages = {
-        Sun: 'https://images-assets.nasa.gov/image/GSFC_20171208_Archive_e001435/GSFC_20171208_Archive_e001435~orig.jpg',
-        Mercury: 'https://images-assets.nasa.gov/image/PIA11245/PIA11245~orig.jpg',
-        Venus: 'https://images-assets.nasa.gov/image/PIA00270/PIA00270~orig.jpg',
-        Earth: 'https://images-assets.nasa.gov/image/PIA18033/PIA18033~orig.jpg',
-        Mars: 'https://images-assets.nasa.gov/image/PIA03276/PIA03276~orig.jpg',
-        Jupiter: 'https://images-assets.nasa.gov/image/PIA00343/PIA00343~orig.jpg',
-        Saturn: 'https://images-assets.nasa.gov/image/PIA05380/PIA05380~orig.jpg',
-        Uranus: 'https://images-assets.nasa.gov/image/PIA18182/PIA18182~orig.jpg',
-        Neptune: 'https://images-assets.nasa.gov/image/PIA01492/PIA01492~orig.jpg',
-        Pluto: 'https://images-assets.nasa.gov/image/PIA19952/PIA19952~orig.jpg'
-    }
-
-    let planetDescription = {
-        Sun: 'The Sun is the star at the center of the Solar System. It is a nearly perfect sphere of hot plasma and is by far the most important source of energy for life on Earth.',
-        Mercury: 'Mercury is the smallest and innermost planet in the Solar System.  It is named after the Roman deity Mercury, the messenger of the gods.',
-        Venus: 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty. As the second-brightest natural object in the night sky after the Moon, Venus can cast shadows and, rarely, is visible to the naked eye in broad daylight.',
-        Earth: 'Earth is the third planet from the Sun and the only astronomical object known to harbor life. According to radiometric dating and other evidence, Earth formed over 4.5 billion years ago.',
-        Mars: 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System after Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the "Red Planet".',
-        Jupiter: 'Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant named after the Roman god Jupiter. When viewed from Earth, Jupiter is on average the third-brightest natural object in the night sky after the Moon and Venus. ',
-        Saturn: 'Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine times that of Earth. Saturn is named after the Roman god of wealth and agriculture.',
-        Uranus: 'Uranus is the seventh planet from the Sun. It is similar in composition to Neptune, and both have bulk chemical compositions which differ from that of the larger gas giants Jupiter and Saturn.',
-        Neptune: 'Neptune is the eighth and farthest known planet from the Sun in the Solar System. In the Solar System, it is the fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. It is named after the Roman god of the sea.',
-        Pluto: 'Pluto is an icy dwarf planet in the Kuiper belt, a ring of bodies beyond the orbit of Neptune. It was the first and the largest Kuiper belt object to be discovered.'
-    }
-
     const chooseImg = (name) => {
-        return planetImages[name]
+        return planets[name].image
     }
 
     const chooseDescription = name => {
-        return planetDescription[name]
+        return planets[name].description
     }
 
     const howManyEarths = (planet, value, exponent) => {
@@ -57,7 +32,12 @@ const SolarSystemCard = (props) => {
                 {/* planet image and description */}
                 <div className="firstInfo">
                     <img className="planet-img" src={chooseImg(props.planetName)} alt="picture of planet" />
-                    <p className="planet-desc">{chooseDescription(props.planetName)} <br /> <a href={`https://en.wikipedia.org/wiki/${props.planetName}`} target="_blank">Read More</a></p>
+                    <p className="planet-desc">
+                        {chooseDescription(props.planetName)} <br />
+                        {props.planetName === "Mercury" ?
+                            <a href={`https://en.wikipedia.org/wiki/${props.planetName}_(planet)`} target="_blank">Read More</a>
+                            : <a href={`https://en.wikipedia.org/wiki/${props.planetName}`} target="_blank">Read More</a>}
+                    </p>
                 </div>
                 {
                     props.planetName !== 'Sun' ?
@@ -66,7 +46,7 @@ const SolarSystemCard = (props) => {
                             <p>Moons: {props.numberOfMoons} <br /> {props.numberOfMoons > 1 ? <a href="">See Moons</a> : props.numberOfMoons === 1 ? <a href="">See Moon</a> : ''}</p>
                             {/* sideral orbit and rotation */}
                             <p>Year length: {Math.round(props.sideralOrbit)} days</p>
-                            <p>Day length: {Math.round(props.sideralRotation)} hours</p>
+                            <p>Day length: {Math.round(Math.abs(props.sideralRotation))} hours</p>
                         </div>
                         : ''
                 }
